2a60e9fbad2f65e9218cd640cf3ba7cec59c2373
hong-gyeonghyeon <wertys5674@naver.com>
2021-07-09 18:11:08 +0900

    putBomb putInitBomb 수정하기

--- src/Bingo/Pick.java
+++ src/Bingo/Pick.java
@@ -6,7 +6,6 @@ public class Pick {
     Scanner scanner = new Scanner(System.in);
 
     int pickX(){
-        //XXX: x&y 5 초과하면 에러발생
         int x = Integer.parseInt(scanner.next());
         return x-1;
     }
